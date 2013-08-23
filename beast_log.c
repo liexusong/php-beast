a8ae9e70d3f696e34d6b765276beded5b3c7d486
liexusong <liexusong@qq.com>
2013-08-23 14:02:38 +0800

    update log feature

--- beast_log.c
+++ beast_log.c
@@ -9,11 +9,9 @@ extern char *beast_log_file;
 
 void beast_write_log(beast_log_level level, const char *fmt, ...)
 {
-    char *headers[] = {"{debug}", "{notice}", "{error}"};
+    char *headers[] = {"[debug]", "[notice]", "[error]"};
     va_list ap;
     FILE *fp;
-    char buf[64];
-    time_t now;
 
     if (level > beast_log_error ||
         level < beast_log_debug) {
@@ -27,12 +25,11 @@ void beast_write_log(beast_log_level level, const char *fmt, ...)
 
     va_start(ap, fmt);
 
-    now = time(NULL);
-    strftime(buf, 64, "[%d %b %H:%M:%S]", gmtime(&now));
-    fprintf(fp, "%s %s ", buf, headers[level]);
+    fprintf(fp, "%s ", headers[level]);
     vfprintf(fp, fmt, ap);
     fprintf(fp, "\n");
     fflush(fp);
+
     va_end(ap);
 
     fclose(fp);
--- beast_log.h
+++ beast_log.h
@@ -2,9 +2,9 @@
 #define BEAST_LOG_H
 
 typedef enum {
-    beast_log_debug,
-    beast_log_notice,
-    beast_log_error
+    beast_log_debug,  /* 0 */
+    beast_log_notice, /* 1 */
+    beast_log_error   /* 2 */
 } beast_log_level;
 
 void beast_write_log(beast_log_level level, const char *fmt, ...);
