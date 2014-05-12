2ae883752a9af3512f25fb2d8ba08325141d6fb7
unknown <liexusong@qq.com>
2014-05-13 00:18:49 +0800

    update log feature

--- beast_log.c
+++ beast_log.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include "main/php_reentrancy.h"
 #include "beast_log.h"
 
 static FILE *beast_log_fp = NULL;
@@ -22,7 +23,10 @@ int beast_log_init(char *log_file)
 
 void beast_write_log(beast_log_level level, const char *fmt, ...)
 {
-    char *headers[] = {"[debug]", "[notice]", "[error]"};
+    struct tm local_tm, *result_tm;
+    time_t the_time;
+    char buf[64];
+    char *headers[] = {"DEBUG", "NOTICE", "ERROR"};
     va_list ap;
 
     if (beast_log_fp == NULL || level > beast_log_error ||
@@ -33,12 +37,17 @@ void beast_write_log(beast_log_level level, const char *fmt, ...)
 
     va_start(ap, fmt);
 
-    fprintf(beast_log_fp, "%s ", headers[level]);
+    the_time = time(NULL);
+    result_tm = php_localtime_r(&the_time, &local_tm);
+    strftime(buf, 64, "%d %b %H:%M:%S", result_tm);
+
+    fprintf(beast_log_fp, "[%s] %s: ", buf, headers[level]);
     vfprintf(beast_log_fp, fmt, ap);
     fprintf(beast_log_fp, "\n");
     fflush(beast_log_fp);
 
     va_end(ap);
+
     return;
 }
 
