17ee2d69a0440e77d2708d7761ac06fa83224025
xusong.lie <xusong.lie@vipshop.com>
2016-05-17 14:56:58 +0800

    add aes encrypt module

---
+++ aes_algo_handler.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <string.h>
+#include "beast_module.h"
+#include "aes_algo_lib.c"
+
+static uint8_t key[] = {
+	0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+	0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
+};
+
+int aes_encrypt_handler(char *inbuf, int len,
+	char **outbuf, int *outlen)
+{
+	int blocks, i;
+	char *out;
+	char in[16];
+
+	blocks = len / 16;
+	if (len % 16) { /* not enough one block (16 bytes) */
+		blocks += 1;
+	}
+
+	out = malloc(blocks * 16);
+	if (!out) {
+		return -1;
+	}
+
+	for (i = 0; i < blocks; i++) {
+		int size;
+
+		memset(in, 0, 16);
+
+		if (i == blocks - 1 && (len % 16)) { /* the last block */
+			size = len % 16;
+		} else {
+			size = 16;
+		}
+
+		memcpy(in, inbuf + i * 16, size);
+
+		AES128_ECB_encrypt(in, key, out + i * 16);
+	}
+
+	*outbuf = out;
+	*outlen = blocks * 16;
+
+	return 0;
+}
+
+
+int aes_decrypt_handler(char *inbuf, int len,
+	char **outbuf, int *outlen)
+{
+	int blocks, i;
+	char *out;
+
+	if (len % 16) {
+		return -1;
+	}
+
+	blocks = len / 16;
+
+	out = malloc(blocks * 16);
+	if (!out) {
+		return -1;
+	}
+
+	for (i = 0; i < blocks; i++) {
+		AES128_ECB_decrypt(inbuf + i * 16, key, out + i * 16);
+	}
+
+	*outbuf = out;
+	*outlen = blocks * 16;
+
+	return 0;
+}
+
+
+void aes_free_handler(void *ptr)
+{
+    if (ptr) {
+        free(ptr);
+    }
+}
+
+struct beast_ops aes_handler_ops = {
+	.name = "aes-algo",
+	.encrypt = aes_encrypt_handler,
+	.decrypt = aes_decrypt_handler,
+	.free = aes_free_handler,
+};
--- global_algo_modules.c
+++ global_algo_modules.c
@@ -3,9 +3,11 @@
 
 extern struct beast_ops des_handler_ops;
 extern struct beast_ops base64_handler_ops;
+extern struct beast_ops aes_handler_ops;
 
 struct beast_ops *ops_handler_list[] = {
 	&des_handler_ops,
 	&base64_handler_ops,
+	&aes_handler_ops,
 	NULL,
 };
