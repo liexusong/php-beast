8fb15c381283bbe4883e0f0feabb6b8350864814
xusong.lie <xusong.lie@vipshop.com>
2016-05-20 14:02:33 +0800

    update file header

--- header.c
+++ header.c
@@ -3,7 +3,8 @@
  * You can modify this sign to disguise your encrypt file
  */
 char encrypt_file_header_sign[] = {
-	0xe8, 0x16, 0xa4, 0x0c
+	0xe8, 0x16, 0xa4, 0x0c,
+	0xf2, 0xb2, 0x60, 0xee
 };
 
 int encrypt_file_header_length = sizeof(encrypt_file_header_sign);
