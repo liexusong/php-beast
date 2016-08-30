3dd2b87b70133b29e5ab29f4de5f89b734d7ebc2
xusong.lie <xusong.lie@vipshop.com>
2016-08-30 14:31:10 +0800

    add beast_file_expire() function

--- php_beast.h
+++ php_beast.h
@@ -44,6 +44,7 @@ PHP_FUNCTION(beast_encode_file);
 PHP_FUNCTION(beast_avail_cache);
 PHP_FUNCTION(beast_cache_info);
 PHP_FUNCTION(beast_support_filesize);
+PHP_FUNCTION(beast_file_expire);
 
 /*
   	Declare any global variables you may need between the BEGIN
