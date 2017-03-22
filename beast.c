c3a4abcf9166ee34c10232c1622ff44781e3b3b2
liexusong <liexusong@qq.com>
2017-03-23 01:59:34 +0800

    fix bugs

--- beast.c
+++ beast.c
@@ -12,7 +12,8 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: Liexusong <280259971@qq.com>                                 |
+  | Author: Liexusong <liexusong@qq.com>                                 |
+  |         maben <www.maben@foxmail.com>                                |
   +----------------------------------------------------------------------+
 */
 
