db182936b39a17b29a6d1568089853ed780f863e
xusong.lie <xusong.lie@vipshop.com>
2017-03-24 16:56:56 +0800

    optimize spinlock on windows

--- spinlock.c
+++ spinlock.c
@@ -52,12 +52,12 @@ void beast_spinlock(beast_atomic_t *lock, int pid)
 
             for (n = 1; n < 129; n << 1) {
 
-                for (i = 0; i < n; i++) {
-                    pause();
-                }
-
                 if (compare_and_swap(lock, 0, pid)) {
                     return;
+                }
+
+                for (i = 0; i < n; i++) {
+                    pause();
                 }
             }
         }
